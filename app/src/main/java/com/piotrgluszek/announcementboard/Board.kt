e1e8d7be9960cdea6b85dbbcafe449ea8b0cfdef
Piotr Głuszek <piogluszek@gmail.com>
2018-11-18 20:58:42 +0100

    Add login and register user activities

---
+++ app/src/main/java/com/piotrgluszek/announcementboard/Board.kt
@@ -0,0 +1,12 @@
+package com.piotrgluszek.announcementboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Board : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board)
+    }
+}
---
+++ app/src/main/java/com/piotrgluszek/announcementboard/UserRegistration.kt
@@ -0,0 +1,12 @@
+package com.piotrgluszek.announcementboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserRegistration : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_registration)
+    }
+}
